43bd6c6f42ad46c09b8e00b68bd6208872879d84
Tiago Martins <tiago86martins@gmail.com>
2025-07-08 14:52:22 +0100

    Deploy script

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -146,7 +146,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
         s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(uint256 /* requestId */, uint256[] calldata randomWords) internal override {
+    function fulfillRandomWords(uint256, /* requestId */ uint256[] calldata randomWords) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
